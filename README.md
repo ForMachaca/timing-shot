714c715b290b93353e9c3358ca3ec66442762db8
yjh <1023457761@qq.com>
2022-07-12 22:14:45 +0800

    定时执行

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ pip install -r requirements.txt
 ## 输出项目使用的库
 pip freeze > requirements.txt  
 ## build .exe  
-pip3 install pyinstaller
+pip3 install pyinstaller / python -m pip install  pyinstaller
 pyinstaller -F main.py 
 
 ```
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+'''
+Author: y
+Date: 2022-07-12 19:34:38
+LastEditors: y
+LastEditTime: 2022-07-12 21:16:34
+'''
 # -*- coding: UTF-8 -*-
 
 import paramiko
 from PIL import ImageGrab
 import threading
 import time
+import datetime
 import os
 import requests
 from pathlib import Path
@@ -22,7 +29,7 @@ def func():
     pic.mode = 'RGB'
     pic.save("images/" + pic_name)
     # pic.show()
-    print("show")
+    print("show, now: ", datetime.datetime.now())
 
     # 通过sftp文件上传
     # ssh = paramiko.SSHClient()
@@ -35,18 +42,18 @@ def func():
     # ssh.exec_command("python /www/wwwroot/ocr.py")  # 执行服务器上相应脚本
 
     # 模拟浏览器POST文件上传
-    files = {'file': (pic_name, open("images/" + pic_name, 'rb'), 'image/png')}
-    data = {
-        # "computer": 1
-    }
-    result = requests.post(url='http://127.0.0.1:5000/index', data=data, files=files, headers={})
-    print(result.text)
-    os.remove("images/" + pic_name)
+    # files = {'file': (pic_name, open("images/" + pic_name, 'rb'), 'image/png')}
+    # data = {
+    #     # "computer": 1
+    # }
+    # result = requests.post(url='http://127.0.0.1:5000/index', data=data, files=files, headers={})
+    # print(result.text)
+    # os.remove("images/" + pic_name)
 
 
     # 开始定时任务
-    # timer = threading.Timer(60, func, [])
-    # timer.start()
+    timer = threading.Timer(60, func, [])
+    timer.start()
 
 
 func()
