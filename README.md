55a88f2ee5dbeba1c12add02286333663cb9da9c
yuanjiahui <1023457761@qq.com>
2022-07-13 18:27:08 +0800

    增加窗口判断

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+Spy++ 获取窗口 类名，标题
 ```
 ## install
 pip install -r requirements.txt
@@ -6,6 +7,7 @@ pip freeze > requirements.txt
 ## build .exe  
 pip3 install pyinstaller / python -m pip install  pyinstaller
 pyinstaller -F main.py 
-
+## 进入venv
+venv/bin/activate
 ```
 
--- main.py
+++ main.py
@@ -6,25 +6,66 @@ LastEditTime: 2022-07-12 21:16:34
 '''
 # -*- coding: UTF-8 -*-
 
-import paramiko
-from PIL import ImageGrab
-import threading
-import time
 import datetime
 import os
+import paramiko
 import requests
+import threading
+import time
+import win32api
+import win32con
+import win32gui
+from PIL import ImageGrab
 from pathlib import Path
 
 
+def match_windows(win_title):
+    """
+    查找指定窗口
+    :param win_title: 窗口名称
+    :return: 句柄列表
+    """
+
+    def callback(hwnd, hwnds):
+        if win32gui.IsWindowVisible(hwnd) and win32gui.IsWindowEnabled(hwnd):
+            win_text = win32gui.GetWindowText(hwnd)
+            # 模糊匹配
+            if win_text.find(win_title) > -1:
+                hwnds.append(hwnd)
+        return True
+
+    hwnds = []
+    win32gui.EnumWindows(callback, hwnds)  # 列出所有顶级窗口，并传递它们的指针给callback函数
+    return hwnds
+
+
+# 获取窗口信息
+def get_window_pos(name):
+    name = name
+    handle = win32gui.FindWindow(0, name)    # 类名，标题
+    # 获取窗口句柄
+    if handle == 0:
+        return None
+    else:
+        # 返回坐标值和handle
+        return win32gui.GetWindowRect(handle), handle
+
+
 def func():
+    # 获取坐标
+    (x1, y1, x2, y2), handle = get_window_pos('vscode')
+    # 发送还原最小化窗口的信息
+    # win32gui.SendMessage(handle, win32con.WM_SYSCOMMAND, win32con.SC_RESTORE, 0)
+    # 设为高亮
+    win32gui.SetForegroundWindow(handle)
     # 目录不存在，则创建截图存放的目录
     if Path("images").is_dir() != 1:
         os.mkdir("images")
 
-        # 开始截图
-    # pic = ImageGrab.grab((200, 100, 1000, 800))  # 指定截取坐标(左边X，上边Y，右边X，下边Y)
+    # 开始截图
     # using the grab method
-    pic = ImageGrab.grab(None)
+    pic = ImageGrab.grab((x1, y1, x2, y2))  # 指定截取坐标(左边X，上边Y，右边X，下边Y)
+    # pic = ImageGrab.grab(None)
     pic_name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
     pic.mode = 'RGB'
     pic.save("images/" + pic_name)
@@ -46,10 +87,9 @@ def func():
     # data = {
     #     # "computer": 1
     # }
-    # result = requests.post(url='http://127.0.0.1:5000/index', data=data, files=files, headers={})
+    # result = requests.post(url='http://127.0.0.1:5000/upload/img', data=data, files=files, headers={})
     # print(result.text)
     # os.remove("images/" + pic_name)
-
 
     # 开始定时任务
     timer = threading.Timer(60, func, [])
